b0fd71daff7edecee04f5bb59f748d21fd0f30b5
KID <50666207+KID421@users.noreply.github.com>
2019-11-12 21:00:14 +0800

    添加修飾詞

--- UnityProject/Assets/Scripts/First.cs
+++ UnityProject/Assets/Scripts/First.cs
@@ -5,9 +5,11 @@ public class First : MonoBehaviour
 {
     // 定義欄位 Field (宣告變數)
     // 語法：
-    // 欄位類型 欄位名稱 結束
-    int level;      // 整數
-    float speed;    // 浮點數
-    string prop;    // 字串
-    bool mission;   // 布林值
+    // 修飾詞 欄位類型 欄位名稱 結束
+    // 私人 private   - 隱藏(預設)
+    // 公開 public    - 顯示
+    public int level;      // 整數
+    public float speed;    // 浮點數
+    public string prop;    // 字串
+    public bool mission;   // 布林值
 }
