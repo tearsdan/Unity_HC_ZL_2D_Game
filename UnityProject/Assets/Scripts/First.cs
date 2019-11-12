172a8b1204224e1bf6554da5cd429ce5cd9c2052
KID <50666207+KID421@users.noreply.github.com>
2019-11-12 20:48:57 +0800

    認識欄位

---
+++ UnityProject/Assets/Scripts/First.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;  // 引用 Unity API
+
+// 類別 類別名稱
+public class First : MonoBehaviour
+{
+    // 定義欄位 Field (宣告變數)
+    // 語法：
+    // 欄位類型 欄位名稱 結束
+    int level;      // 整數
+    float speed;    // 浮點數
+    string prop;    // 字串
+    bool mission;   // 布林值
+}
