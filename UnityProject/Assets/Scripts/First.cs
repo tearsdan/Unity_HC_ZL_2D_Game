73fbe36710808700af1b3a0c654291e07006e869
KID <50666207+KID421@users.noreply.github.com>
2019-11-12 21:10:30 +0800

    添加指定值

--- UnityProject/Assets/Scripts/First.cs
+++ UnityProject/Assets/Scripts/First.cs
@@ -5,11 +5,11 @@ public class First : MonoBehaviour
 {
     // 定義欄位 Field (宣告變數)
     // 語法：
-    // 修飾詞 欄位類型 欄位名稱 結束
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
     // 私人 private   - 隱藏(預設)
     // 公開 public    - 顯示
-    public int level;      // 整數
-    public float speed;    // 浮點數
-    public string prop;    // 字串
-    public bool mission;   // 布林值
+    public int level = 1;               // 整數 預設 0
+    public float speed = 10.5f;         // 浮點數 預設 0
+    public string prop = "藍色藥水";    // 字串 預設 ""
+    public bool mission = true;         // 布林值 預設 false
 }
