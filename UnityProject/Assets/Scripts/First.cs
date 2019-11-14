3fe0c403b93b6f4dcab129b76c45e16637daebe7
KID <50666207+KID421@users.noreply.github.com>
2019-11-14 19:05:22 +0800

    認識欄位屬性：標題、提示與範圍

--- UnityProject/Assets/Scripts/First.cs
+++ UnityProject/Assets/Scripts/First.cs
@@ -12,4 +12,19 @@ public class First : MonoBehaviour
     public float speed = 10.5f;         // 浮點數 預設 0
     public string prop = "藍色藥水";    // 字串 預設 ""
     public bool mission = true;         // 布林值 預設 false
+
+    // 屬性
+    // 語法：[屬性名稱(屬性內容)]
+    // [標題("標題內容")]
+    [Header("血量")]
+    public int hp = 100;
+    [Header("跳躍高度")]
+    public float jump = 99.9f;
+    // [提示("提示內容")]
+    [Header("死亡")]
+    [Tooltip("用來紀錄玩家是否死亡，打勾：死亡、取消：沒死。")]
+    public bool dead;
+    // [範圍(最小值，最大值)]
+    [Header("魔力"),Range(0, 999)]
+    public int mp = 50;
 }
