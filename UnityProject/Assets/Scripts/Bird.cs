2efd0ccb80fd0efaab5a8714a8e98e71dd24c56c
KID <50666207+KID421@users.noreply.github.com>
2019-11-14 19:38:15 +0800

    練習：小雞、地板與遊戲管理器

---
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [Header("跳躍高度"), Range(50, 2000)]
+    public int jump = 100;
+    [Header("是否死亡")]
+    public bool dead;
+}
---
+++ UnityProject/Assets/Scripts/Ground.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [Header("速度"), Range(0, 100)]
+    public float speed = 10.5f;
+}
