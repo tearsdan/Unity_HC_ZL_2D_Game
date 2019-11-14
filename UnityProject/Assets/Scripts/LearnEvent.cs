ddc311bc9496504dcdb43ac9dd9d22632de9c61b
KID <50666207+KID421@users.noreply.github.com>
2019-11-14 20:30:06 +0800

    認識事件：開始事件

---
+++ UnityProject/Assets/Scripts/LearnEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LearnEvent : MonoBehaviour
+{
+    // 事件：在特定時間點以指定次數執行
+    // 開始事件：播放後執行一次
+    // 語法：
+    // 修飾詞 傳回類型 名稱() {  }
+    private void Start()
+    {
+        // 輸出方法(物件);
+        print("哈囉，沃德~ :P");
+    }
+}
