aa70b1f34f537d7a106c4032e0ceca7f0fb72612
KID <50666207+KID421@users.noreply.github.com>
2019-11-14 21:10:37 +0800

    定義方法與呼叫方法

---
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    // 定義方法 Method (宣告函式、功能)
+    // 語法：
+    // 修飾詞 方法傳回類型 方法名稱 () {  }
+    // void 無傳回
+    private void Test()
+    {
+
+    }
+
+    private void Drive()
+    {
+        print("車子正在往前開，時速：999");
+    }
+
+    private void Start()
+    {
+        // 呼叫方法
+        Drive();
+    }
+}
