db382ee5fb761498dc5d552a570273c845d8165e
KID <50666207+KID421@users.noreply.github.com>
2019-11-19 18:49:14 +0800

    認識方法參數與呼叫方法填入引數

--- UnityProject/Assets/Scripts/LearnMethod.cs
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -4,7 +4,8 @@ public class LearnMethod : MonoBehaviour
 {
     // 定義方法 Method (宣告函式、功能)
     // 語法：
-    // 修飾詞 方法傳回類型 方法名稱 () {  }
+    // 修飾詞 方法傳回類型 方法名稱 (參數) {  }
+    // 參數語法：參數類型 參數名稱
     // void 無傳回
     private void Test()
     {
@@ -12,17 +13,21 @@ public class LearnMethod : MonoBehaviour
     }
 
     /// <summary>
-    /// 開車子。
+    /// 開車
     /// </summary>
-    private void Drive()
+    /// <param name="speed">車子的速度</param>
+    private void Drive(int speed)
     {
-        print("車子正在往前開，時速：999");
+        print("引擎音效");
+        // 字串可以使用 + 號與其他形態串接，結果為字串
+        print("車子正在往前開，時速：" + speed);
     }
 
     private void Start()
     {
-        // 呼叫方法
-        Drive();
-        Drive();
+        // 呼叫方法，給予對應的引數
+        Drive(99);
+        Drive(999);
+        Drive(9999999);
     }
 }
