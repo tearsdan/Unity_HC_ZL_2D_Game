0867a7be7d840a5e8c419d0052ffcd888d7cd528
KID <50666207+KID421@users.noreply.github.com>
2019-11-21 21:19:13 +0800

    添加隨機 Y 軸

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -6,6 +6,29 @@ public class GameManager : MonoBehaviour
     public int score;
     [Header("最高分數")]
     public int scoreHeight;
+    [Header("水管")]
+    public GameObject pipe; // GameObject 可以存放預製物以及場景上的物件
+
+    /// <summary>
+    /// 生成水管功能
+    /// </summary>
+    public void SpawnPipe()
+    {
+        // 因為有繼承此類別才可簡寫
+        // Object.Instantiate(pipe);
+
+        // 浮點數 = 隨機.靜態方法(最小，最大)
+        float y = Random.Range(-1.1f, 1.9f);
+
+        // 三維向量 Vector3 - x, y, z
+        Vector3 pos = new Vector3(6f, y, 0);
+
+        // 四元數 Quaternion - x, y, z, w
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+
+        // 生成(物件，座標，角度)
+        Instantiate(pipe, pos, rot);
+    }
 
     /// <summary>
     /// 加分功能
@@ -30,5 +53,10 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
 
+    }
+
+    private void Start()
+    {
+        SpawnPipe();
     }
 }
---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        print("輸出!");
+
+
+        // 靜態屬性：類別.屬性
+        print(Random.value);
+
+        // 練習取得 PI
+        print(Mathf.PI);
+
+        // 靜態方法：類別.方法(對應的參數);
+        print("隨機範圍：" + Random.Range(50.5f, 100.9f));
+
+        // object 任何類型：int、float、string、bool
+        Debug.Log(true);
+        Debug.LogWarning("警告");
+        Debug.LogError("錯誤");
+    }
+
+    private void Update()
+    {
+        // 輸入.按鍵名稱
+        print(Input.inputString);
+
+        // 練習：請練習偵測玩家有沒有按 s
+        print(Input.GetKeyDown("s"));
+    }
+}
