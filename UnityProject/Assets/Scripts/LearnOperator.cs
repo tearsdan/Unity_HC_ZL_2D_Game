417b8c0b96d32904dfd36fa70df46246f6c68179
KID <50666207+KID421@users.noreply.github.com>
2019-11-26 21:23:01 +0800

    認識運算子：比較運算子

--- UnityProject/Assets/Scripts/LearnOperator.cs
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -3,6 +3,7 @@
 public class LearnOperator : MonoBehaviour
 {
     public int a = 10, b = 3;
+    public int c = 9, d = 1;
 
     private void Start()
     {
@@ -17,5 +18,17 @@ public class LearnOperator : MonoBehaviour
         a = a + 1;      // 等號右邊先執行再存回左邊
         print(a++);     // 先輸出再執行遞增
         print(++a);     // 先執行遞增再輸出
+
+        b = b + 10;
+        b += 10;        // 適用 + - * / %
+
+        // 比較運算子
+        // 結果是布林值
+        print(c > d);       // true
+        print(c < d);       // false
+        print(c >= d);      // true
+        print(c <= d);      // false
+        print(c == d);      // false
+        print(c != d);      // true
     }
 }
