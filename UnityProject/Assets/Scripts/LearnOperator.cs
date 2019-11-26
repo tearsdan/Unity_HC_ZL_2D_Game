e6d55322dd87573c9982d2d0313463dceba6c621
KID <50666207+KID421@users.noreply.github.com>
2019-11-26 20:51:37 +0800

    認識運算子：數學運算子 - 加減乘除餘

---
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int a = 10, b = 3;
+
+    private void Start()
+    {
+        // 數學運算子
+        // + - * / %
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+    }
+}
