1d18dc474744386c22727a0767b5e43cb5cd6f04
KID <50666207+KID421@users.noreply.github.com>
2019-11-26 20:03:24 +0800

    練習：地板移動以及利用一幀的時間解決不同電腦執行速度不同問題

--- UnityProject/Assets/Scripts/Ground.cs
+++ UnityProject/Assets/Scripts/Ground.cs
@@ -5,11 +5,20 @@ public class Ground : MonoBehaviour
     [Header("速度"), Range(0, 100)]
     public float speed = 10.5f;
 
+    [Header("變形")]
+    public Transform tran;
+
     /// <summary>
     /// 地板移動功能
     /// </summary>
     private void Move()
     {
+        // Time.deltaTime 一幀的時間
+        tran.Translate(-speed * Time.deltaTime, 0, 0);
+    }
 
+    private void Update()
+    {
+        Move();
     }
 }
