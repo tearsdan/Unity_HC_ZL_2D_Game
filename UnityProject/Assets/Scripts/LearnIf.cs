2109c2f314329e97f93f2df7c3930eaa1d5082ad
KID <50666207+KID421@users.noreply.github.com>
2019-11-28 19:30:46 +0800

    判斷式關鍵字：else 否則

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool open;
+
+    private void Start()
+    {
+        // 判斷式 if (switch)
+        // 語法：
+        // if (布林值) { 當 if () 布林值為 true 執行這裡 }
+
+        if (true)
+        {
+            print("嗨~");
+        }
+    }
+
+    private void Update()
+    {
+        // if () 布林值為 true 會執行 if {}
+        if (open)
+        {
+            print("開門!");
+        }
+        // if () 布林值為 false 會執行 else {} (否則)
+        else
+        {
+            print("關門!");
+        }
+    }
+}
