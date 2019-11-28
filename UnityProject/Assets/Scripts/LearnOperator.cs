22bcb1a0d322b78d1270611872623ffcae94be93
KID <50666207+KID421@users.noreply.github.com>
2019-11-28 19:05:12 +0800

    認識運算子：顛倒運算子與邏輯運算子應用

--- UnityProject/Assets/Scripts/LearnOperator.cs
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -5,6 +5,12 @@ public class LearnOperator : MonoBehaviour
     public int a = 10, b = 3;
     public int c = 9, d = 1;
 
+    public int apple = 5;
+    public int coin = 3;
+
+    public bool key;
+    public int boom = 5;
+
     private void Start()
     {
         // 數學運算子
@@ -30,5 +36,32 @@ public class LearnOperator : MonoBehaviour
         print(c <= d);      // false
         print(c == d);      // false
         print(c != d);      // true
+
+        // 邏輯運算子
+        // 結果是布林值
+        // 並且 &&
+        // 規則：只要有一個 false 就會傳回 false
+        print(true && true);    // true
+        print(true && false);   // false
+        print(false && true);   // false
+        print(false && false);  // false
+
+        // 或者 ||
+        // 規則：只要有一個 true 就會傳回 true
+        print(true || true);    // true
+        print(true || false);   // true
+        print(false || true);   // true
+        print(false || false);  // false
+
+        // 相反 (顛倒) !
+        // 作用：將布林值變相反
+        print(!true);
+        print(!false);
+
+        // 任務：取得十個蘋果與五個金幣
+        print(apple >= 10 && coin >= 5);
+
+        // 任務：取得一個鑰匙或者兩個炸彈
+        print(key && boom >= 2);
     }
 }
